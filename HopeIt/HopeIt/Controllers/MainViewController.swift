96437bbe8fb414f306d897f10ba33faef994e334
pawelqus <pawelqus@gmail.com>
2017-10-27 22:30:18 +0200

    Add braintree

---
+++ HopeIt/HopeIt/Controllers/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  HopeIt
+//
+//  Created by Paweł Nużka on 27/10/2017.
+//  Copyright © 2017 Paweł Nużka. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet var tipAmount10: UIButton!
+    @IBOutlet var tipAmount30: UIButton!
+    @IBOutlet var tipAmount50: UIButton!
+    
+    var selectedIndex: Int = -1
+    
+    
+    
+}
